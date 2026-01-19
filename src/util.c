7fde1dd071882b8bd2b95731674a1a2c5b777782
Ladipo Ezekiel Ipadeola <rileydrizzy@hotmail.com>
2026-01-19 01:58:40 +0000

    feat: add er_mode argument to enable emergency admission

--- src/util.c
+++ src/util.c
@@ -4,33 +4,42 @@
 #include <stdlib.h>
 #include <string.h>
 
-Patient_Node *create_patient_node(Patient_Queue *queue)
+Patient_Node *create_patient_node(Patient_Queue *queue, bool er_mode)
 {
-  Patient_Node *Patient = calloc(1, sizeof(Patient_Node));
-  if (Patient == NULL)
+  Patient_Node *patient = calloc(1, sizeof(Patient_Node));
+  if (patient == NULL)
   {
     // TODO Implement a error val
     puts("Allocation failed");
     return NULL;
   }
-  Patient->next_patient = NULL;
+  patient->next_patient = NULL;
   if (queue->head == NULL)
   {
-    queue->head = Patient;
-    queue->tail = Patient;
-    return Patient;
+    queue->head = patient;
+    queue->tail = patient;
+    return patient;
   }
   else
   {
-    queue->tail->next_patient = Patient;
-    queue->tail = Patient;
+    if (er_mode)
+    {
+      patient->next_patient = queue->head;
+      queue->head = patient;
+      return patient;
+    }
+    else
+    {
+      queue->tail->next_patient = patient;
+      queue->tail = patient;
+      return patient;
+    }
   }
-  return Patient;
 }
 
-void register_patient(WINDOW *win, int width, int height, Patient_Queue *queue)
+void register_patient(WINDOW *win, int width, Patient_Queue *queue, bool er_mode)
 {
-  Patient_Node *new_Patient = create_patient_node(queue);
+  Patient_Node *new_patient = create_patient_node(queue, er_mode);
 
   // Clear the inside of the existing box
   werase(win);
@@ -45,34 +54,36 @@ void register_patient(WINDOW *win, int width, int height, Patient_Queue *queue)
   curs_set(1); // Show cursor for typing
   // Safe input for strings
   mvwprintw(win, 5, 4, "Enter Patient First Name: ");
-  wgetnstr(win, new_Patient->first_name, 49);
+  wgetnstr(win, new_patient->first_name, 49);
 
   mvwprintw(win, 7, 4, "Enter Patient Last Name: ");
-  wgetnstr(win, new_Patient->last_name, 49);
-
+  wgetnstr(win, new_patient->last_name, 49);
+  // char p_initial;
+  // p_initial = new_patient->last_name[0]; // TODO add p.inital to be show on display
   mvwprintw(win, 9, 4, "Enter Patient Age: ");
   wrefresh(win);
 
   // Using scanw style for age
   char age_str[4];
   wgetnstr(win, age_str, 9);
-  new_Patient->age = atoi(age_str);
+  new_patient->age = atoi(age_str);
 
   // Success Message
   noecho();
   curs_set(0);
   wattron(win, COLOR_PAIR(1) | A_BOLD); // Assuming color 1 is green
-  mvwprintw(win, 12, (width - (18 + strlen(new_Patient->first_name))) / 2,
-            "[Success] %s, %s added to the line.", new_Patient->first_name,
-            new_Patient->last_name);
+  mvwprintw(win, 12, 20, "==[Success]==");
+  mvwprintw(win, 13, (width - (18 + strlen(new_patient->last_name))) / 2,
+            "%s added to the queue.", new_patient->last_name);
   wattroff(win, COLOR_PAIR(1) | A_BOLD);
 
-  mvwprintw(win, 14, (width - 25) / 2, "Press any key to return...");
+  mvwprintw(win, 16, (width - 25) / 2, "Press any key to return...");
   wrefresh(win);
   wgetch(win); // Wait for user to read success message
+  return;
 }
 
-void serving_patient(WINDOW *win, int width, int height, Patient_Queue *queue)
+void serving_patient(WINDOW *win, int width, Patient_Queue *queue)
 {
   werase(win);
   box(win, 0, 0);
@@ -101,7 +112,7 @@ void serving_patient(WINDOW *win, int width, int height, Patient_Queue *queue)
   return;
 }
 
-
+// BUG Segfault when running
 void view_line(WINDOW *win, int width, Patient_Queue *queue)
 {
   Patient_Node *Current = queue->head;
